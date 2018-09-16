782e97cd95b1650e7c26df8712ebe7f1584677a2
ge-hall <Andrew.Ge-Hall@healthcarelogic.com.au>
2018-09-16 21:11:16 +1000

    1. added iso.config.js 2. added index.jsx Not currently working in ISO. Need to track missing dependancies.

---
+++ index.jsx
@@ -0,0 +1,7 @@
+<Box p={3}>
+  <Donut
+    value={1/2}
+    size={256}
+    color='tomato'
+  />
+</Box>
---
+++ iso.config.js
@@ -0,0 +1,9 @@
+// example iso.config.js
+const { Box, Donut } = require('rebass')
+
+module.exports = {
+  components: {
+    Box,
+    Donut
+  }
+}
